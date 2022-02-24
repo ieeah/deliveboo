4c0fa19e6f65aa1604e94d25e43bdc29b6749033
ieeah <87276064+ieeah@users.noreply.github.com>
2022-02-24 15:56:38 +0100

    first-configuration

---
+++ resources/views/guests/home.blade.php
@@ -0,0 +1,10 @@
+
+@extends('layouts.app')
+
+@section('content')
+	<h1>
+		Home Page
+	</h1>
+
+	<a href="login">futuro link all'area riservata</a>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// l'home page è temporaneamente statica, poi implementeremo Vue e tutta la parte front-end
+Route::get('/', function() {
+    return view('guests.home');
+})->name('home');
+
+Auth::routes();
+
+Route::middleware('auth')
+    ->namespace('Restaurants')
+    ->name('restaurants.')
+    ->prefix('restaurants')
+    ->group(function() {
+
+});
+
