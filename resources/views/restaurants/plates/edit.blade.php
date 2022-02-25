0aef83f6d81eaeceb339753feaa585e949e894c2
ieeah <87276064+ieeah@users.noreply.github.com>
2022-02-25 17:45:50 +0100

    done client side validation

--- resources/views/restaurants/plates/edit.blade.php
+++ resources/views/restaurants/plates/edit.blade.php
@@ -18,23 +18,23 @@
 			@method('PATCH')
 
 			<div class="mb-4">
-				<label for="name">Nome Piatto</label>
-				<input class="form-control" type="text" name="name" id="name" value="{{ old('title', $plate->name) }}">
+				<label for="name">Nome Piatto *</label>
+				<input class="form-control" type="text" name="name" id="name" value="{{ old('title', $plate->name) }}" required minlength="2" maxlength="200">
 				@error('name')
 					<div class="text-danger fw-bold">{{ $message }}</div>
 				@enderror
 			</div>
 			<div class="mb-4">
 				<label for="description">Descrizione</label>
-				<textarea class="form-control" type="text" name="description" id="description">{{ old('description', $plate->description) }}</textarea>
+				<textarea class="form-control" type="text" name="description" id="description" maxlength="1000">{{ old('description', $plate->description) }}</textarea>
 				@error('description')
 					<div class="text-danger fw-bold">{{ $message }}</div>
 				@enderror
 			</div>
 
 			<div class="mb-4">
-				<label for="ingredients">Ingredienti</label>
-				<textarea class="form-control" type="text" name="ingredients" id="ingredients">{{ old('ingredients', $plate->ingredients) }}</textarea>
+				<label for="ingredients">Ingredienti *</label>
+				<textarea class="form-control" type="text" name="ingredients" id="ingredients" maxlength="1000" required>{{ old('ingredients', $plate->ingredients) }}</textarea>
 				@error('ingredients')
 					<div class="text-danger fw-bold">{{ $message }}</div>
 				@enderror
@@ -42,7 +42,7 @@
 
 			<div class="mb-4">
 				<label for="price">Prezzo</label>
-				<input class="form-control" type="number" name="price" id="price" min="0" step="0.1" value="{{ old('price', $plate->price)}}">
+				<input class="form-control" type="number" name="price" id="price" min="0" max="99.90" required step="0.1" value="{{ old('price', $plate->price)}}">
 			</div>
 
 			<div class="mb-4">
@@ -52,7 +52,7 @@
 
 			<div class="mb-4">
 				<label for="thumb">Cover Image</label>
-				<input class="form-control-file" type="file" name="thumb" id="thumb">
+				<input class="form-control-file" type="file" name="thumb" id="thumb" accept="image/png, image/jpeg">
 				@error('thumb')
 					<div class="text-danger">{{$message}}</div>
 				@enderror
