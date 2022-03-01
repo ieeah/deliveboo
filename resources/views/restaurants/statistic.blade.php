23327461435b2d04d06d2ee6dc5b0858cb6d4f25
dokkalemon <dottori.roberto@gmail.com>
2022-03-01 10:53:44 +0100

    fix monthly chart by years

--- resources/views/restaurants/statistic.blade.php
+++ resources/views/restaurants/statistic.blade.php
@@ -3,14 +3,20 @@
 @section('content')
 
 <main class="statistics container">
+    {{-- <div class="chart">
+        <h3 class="text-primary">Statistiche Mensili</h3>
+        <canvas id="monthlyChart" height="100" width="200"></canvas>
+    </div> --}}
+
     <div class="chart">
         <h3 class="text-primary">Statistiche Mensili</h3>
         <canvas id="monthlyChart" height="100" width="200"></canvas>
-    </div>
 
-    <div class="chart">
-        <h3 class="text-primary">Statistiche Annuali</h3>
-        <canvas id="yearlyChart" height="100" width="200"></canvas>
+        <div class="buttons">
+            <button class="btn btn-primary btn-left"><i class="fa-solid fa-caret-left"></i></button>
+            <div class="btn btn-primary btn-year"></div>
+            <button class="btn btn-primary btn-right"><i class="fa-solid fa-caret-right"></i></button>
+        </div>
     </div>
 
     <div class="chart">
@@ -27,87 +33,127 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
 <script>
 
+
+//Set Year
 const orders = @json($orders);
+console.log(orders);
 
 
+const today = new Date();
+let currentYear = today.getUTCFullYear()
+
+const buttonLeft = document.querySelector('.btn-left');
+const buttonRight = document.querySelector('.btn-right');
+const buttonYear = document.querySelector('.btn-year');
+
+buttonYear.innerHTML = `${currentYear}`
 
 
-//MONTHLY CHART
-const orderMonthly = [];
+//MONTLY CHART
 
+
+filteredOrder = []
+
+
+//Select Order By Year
+function getYearlyOrders() {
+filteredOrder = [];
 for (let i = 0; i < orders.length; i++) {
     const date = new Date(orders[i].created_at);
-    const month = date.getUTCMonth() + 1;
-    orderMonthly.push({
-        month: month,
-    });
+    const year = date.getUTCFullYear();
+    if (year === currentYear) {
+        filteredOrder.push(orders[i])
+    }
+}
 }
 
-const gennaio = [];
-const febbraio = [];
-const marzo = [];
-const aprile = [];
-const maggio = [];
-const giugno = [];
-const luglio = [];
-const agosto = [];
-const settembre = [];
-const ottobre = [];
-const novembre = [];
-const dicembre = [];
+//MONTHLY CHART
+orderMonthly = [];
+let gennaio = [];
+let febbraio = [];
+let marzo = [];
+let aprile = [];
+let maggio = [];
+let giugno = [];
+let luglio = [];
+let agosto = [];
+let settembre = [];
+let ottobre = [];
+let novembre = [];
+let dicembre = [];
 
-orderMonthly.forEach(e => {
-    switch(e.month) {
-        case 1: 
-        gennaio.push(e);
-        break;
+function getMonthlyOrders() {
+    //Reset month Array
+    gennaio = [];
+    febbraio = [];
+    marzo = [];
+    aprile = [];
+    maggio = [];
+    giugno = [];
+    luglio = [];
+    agosto = [];
+    settembre = [];
+    ottobre = [];
+    novembre = [];
+    dicembre = [];
 
-        case 2:
-        febbraio.push(e);
-        break;
+    for (let i = 0; i < filteredOrder.length; i++) {
+        const date = new Date(filteredOrder[i].created_at);
+        const month = date.getUTCMonth() + 1;
+        switch (month) {
+            case 1: 
+            gennaio.push(filteredOrder[i]);
+            break;
 
-        case 3:
-        marzo.push(e);
-        break;
+            case 2:
+            febbraio.push(filteredOrder[i]);
+            break;
 
-        case 4:
-        aprile.push(e);
-        break;
+            case 3:
+            marzo.push(filteredOrder[i]);
+            break;
 
-        case 5:
-        maggio.push(e);
-        break;
+            case 4:
+            aprile.push(filteredOrder[i]);
+            break;
 
-        case 6:
-        giugno.push(e);
-        break;
+            case 5:
+            maggio.push(filteredOrder[i]);
+            break;
 
-        case 7:
-        luglio.push(e);
-        break;
+            case 6:
+            giugno.push(filteredOrder[i]);
+            break;
 
-        case 8:
-        agosto.push(e);
-        break;
+            case 7:
+            luglio.push(filteredOrder[i]);
+            break;
 
-        case 9:
-        settembre.push(e);
-        break;
+            case 8:
+            agosto.push(filteredOrder[i]);
+            break;
 
-        case 10:
-        ottobre.push(e);
-        break;
+            case 9:
+            settembre.push(filteredOrder[i]);
+            break;
 
-        case 11:
-        novembre.push(e);
-        break;
+            case 10:
+            ottobre.push(filteredOrder[i]);
+            break;
 
-        case 12:
-        dicembre.push(e);
-        break;     
+            case 11:
+            novembre.push(filteredOrder[i]);
+            break;
+
+            case 12:
+            dicembre.push(filteredOrder[i]);
+            break;     
+        }
     }
-})
+}
 
+getYearlyOrders();
+getMonthlyOrders();
 
 let monthlyChart = document.getElementById("monthlyChart").getContext('2d');
 
@@ -141,12 +187,71 @@ let massPopChart = new Chart(monthlyChart, {
     },
     options: {
     },
-
-
 })
 
 
+function updateChart() {
+    massPopChart.data.datasets[0].data = {
+        Gennaio: gennaio.length,
+        Febbraio: febbraio.length,
+        Marzo: marzo.length,
+        Aprile: aprile.length,
+        Maggio: maggio.length,
+        Giugno: giugno.length,
+        Luglio: luglio.length,
+        Settembre: settembre.length,
+        Ottobre: ottobre.length,
+        Novembre: novembre.length,
+        Dicembre: marzo.length,
+    };
+    massPopChart.update()
+}
+
+
+
+buttonLeft.addEventListener('click', function() {
+    currentYear-- 
+    buttonYear.innerHTML = `${currentYear}`;
+    getYearlyOrders()
+    getMonthlyOrders();
+    updateChart()
+})
+
+buttonRight.addEventListener('click', function() {
+    currentYear++
+    buttonYear.innerHTML = `${currentYear}`;
+    getYearlyOrders();
+    getMonthlyOrders()
+    updateChart()
+})
+
+
+
+
+
+
+
+
+
+
 //YEARLY CHART
+/* 
+orderYearly = [] */
+
+/* const date = new Date(orders[0].created_at);
+const year = date.getUTCFullYear() + 1;
+ */
+
+/* for (let i = 0; i < orders.length; i++) {
+    const date = new Date(orders[0].created_at);
+    const year = date.getUTCFullYear();
+    orderYearly.push(year);
+}
+
+console.log(orderYearly);
+
+
+
 
 let yearlyChart = document.getElementById("yearlyChart").getContext('2d');
 
@@ -156,7 +261,7 @@ let yearPopChart = new Chart(yearlyChart, {
         datasets: [{
             
             data: {
-                //Insert data
+                
             },
             backgroundColor: [
                 'rgba(255, 20, 0, 0.7)',
@@ -169,7 +274,7 @@ let yearPopChart = new Chart(yearlyChart, {
 })
 
 
-
+ */
 
 
 
