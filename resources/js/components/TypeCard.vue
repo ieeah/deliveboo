47f09bedcd3e0b5f7dff1af53525fc73a79d03bb
ieeah <87276064+ieeah@users.noreply.github.com>
2022-03-04 13:33:55 +0100

    done types-scroller

---
+++ resources/js/components/TypeCard.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="type_card">
+		<!-- <img :src="cover" :alt="name + ' type'"> -->
+		<img src="https://images.unsplash.com/photo-1646243375307-3f6a0b03a664?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60" alt="">
+		<div class="type_name h4">alberto</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'TypeCard',
+	// props: {
+	// 	cover: String,
+	// 	name: String,
+	// },
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../sass/front.scss';
+	.type_card {
+		position: relative;
+		width: clamp(150px, 19%, 300px);
+		flex-shrink: 0;
+		scroll-snap-align: start;
+		img {
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+			object-position: center;
+			filter: brightness(80%);
+			border-radius: .8rem;
+		}
+		.type_name {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			color: $body-bg;
+			font-weight: bold;
+		}
+	}
+</style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -6,21 +6,32 @@
 			</h1>
 			<img src="/storage/front/burger.png" alt="">
 		</div>
-		
-		<div class="types col-xs-12 col-md-9 mx-auto px-xs-3">
-			<h2 class="section_title h2">
-				Di cosa hai voglia stasera?
-			</h2>
-			<div class="type_card ">
 
+		<h2 class="section_title h2 col-md-9 mx-auto">
+			Di cosa hai voglia stasera?
+		</h2>
+		<div class="scrolling_arrows col-md-9 mx-auto">
+			<div class="prev">
+				<i class="fa-solid fa-chevron-left"></i>
 			</div>
+			<div class="next">
+				<i class="fa-solid fa-chevron-right"></i>
+			</div>
+		</div>
+
+		<div class="types col-xs-12 col-md-9 mx-auto px-xs-3">
+			<TypeCard v-for="(n, i) in 7" :key="'type_' + i" />
 		</div>
 	</div>
 </template>
 
 <script>
+import TypeCard from '../components/TypeCard.vue';
 export default {
 	name: 'Home',
+	components: {
+		TypeCard,
+	},
 }
 </script>
 
@@ -49,13 +60,38 @@ export default {
 
 }
 
-.types {
-	.section_title {
+.section_title {
 		color: $primary-400;
-		margin-bottom: 1.5rem;
+		margin-block: 2rem 1.5rem;
 	}
-	.type_card {
 
+	.scrolling_arrows {
+		display: flex;
+		.prev,
+		.next {
+			height: 40px;
+			aspect-ratio: 1;
+			background-color: $primary-400;
+			display: grid;
+			place-content: center;
+			border-radius: 50%;
+			font-size: 1.8rem;
+			color: $body-bg;
+			cursor: pointer;
+		}
+		.prev {
+			margin-right: 1.5rem;
+		}
 	}
+
+.types {
+	display: flex;
+	flex-wrap: nowrap;
+	column-gap: max(2vw, 10%);
+	overflow-x: auto;
+	padding-block: .8rem;
+	scroll-snap-type: x mandatory;
+	scroll-padding: 0 .8rem 0 .8rem;
+
 }
 </style>
