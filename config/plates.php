88a08d5f578ca827d12c4aa9b7553159e54b8c42
Dario0122 <dario.di.cuia@gmail.com>
2022-03-04 18:21:31 +0100

    add plates type

--- config/plates.php
+++ config/plates.php
@@ -137,4 +137,502 @@ return [
 		'ingredients' => 'bucatini, guanciale, pepe, uova, pecorino romano',
 		'visibility' => true,
 	],
+	//italian 1
+	[
+		'name' => 'Lasagne',
+		'ingredients' => 'mozzarella, pomodoro, carne macinata, sale, pepe, parmigiano',
+		'visibility' => true,
+	],[
+		'name' => 'Risotto ai funghi',
+		'ingredients' => 'riso, funghi porcini, sale, pepe',
+		'visibility' => true,
+	],[
+		'name' => 'Polenta',
+		'ingredients' => 'farina di mais, sale',
+		'visibility' => false,
+	],[
+		'name' => 'Tegamino',
+		'ingredients' => 'pomodoro, carne tritata, scamorza, salame, sale, pepe',
+		'visibility' => true,
+	],[
+		'name' => 'Caponata',
+		'ingredients' => 'verdure fritte, pomodoro, aceto, salsa agrodolce, sale',
+		'visibility' => true,
+	],[
+		'name' => 'Spaghetti allo scoglio',
+		'ingredients' => 'pesce misto, pomodoro, sale, pepe',
+		'visibility' => false,
+	],[
+		'name' => 'Cotoletta alla milanese',
+		'ingredients' => 'pollo, uova, farina, sale',
+		'visibility' => true,
+	],[
+		'name' => 'Polpette al pomodoro',
+		'ingredients' => 'carne, pomodoro, uova, pane, sale, pepe',
+		'visibility' => true,
+	],[
+		'name' => 'roast beef',
+		'ingredients' => 'carne manzo, erbe aromatiche, olio Evo, sale, pepe',
+		'visibility' => false,
+	],[
+		'name' => 'Tiramisù',
+		'ingredients' => 'mascarpone, uova, zucchero, savoiardi, caffè, cacao',
+		'visibility' => true,
+	],
+	//italian 2
+	[
+		'name' => 'Pasta al pomodoro',
+		'ingredients' => 'pomodoro, pasta fatta in casa, sale, olio Evo, basilico, parmigiano',
+		'visibility' => true,
+	],[
+		'name' => 'Pasta e rape',
+		'ingredients' => 'rape, pasta fatta in casa, peperoni cruschui, mollica fritta, olio Evo',
+		'visibility' => true,
+	],[
+		'name' => 'Gnocchi alla sorrentina',
+		'ingredients' => 'pomodoro, mozzarella, parmigiano, olio Evo, basilico',
+		'visibility' => true,
+	],[
+		'name' => 'Risotto alla milanese',
+		'ingredients' => 'riso, zafferano, burro, grana padano, sale',
+		'visibility' => true,
+	],[
+		'name' => 'Pasta con le patate',
+		'ingredients' => 'patate, sale, parmigiano, rosmarino, pomodoro',
+		'visibility' => false,
+	],[
+		'name' => 'Pasta col salmone',
+		'ingredients' => 'salmone fresco, panna, olio Evo, sale, pepe',
+		'visibility' => true,
+	],[
+		'name' => 'Tagliata di Angus',
+		'ingredients' => 'carne di manzo, grana padano, rucola, sale, glassa di aceto balsamico',
+		'visibility' => true,
+	],[
+		'name' => 'Polpo alla Luciana',
+		'ingredients' => 'polpo, patate, pomodoro, sale, olio Evo, peperoncino',
+		'visibility' => false,
+	],[
+		'name' => 'Bignè',
+		'ingredients' => 'crema pasticciera, uova, farina, zucchero, burro',
+		'visibility' => true,
+	],[
+		'name' => 'Cannoli siciliani',
+		'ingredients' => 'ricotta di pecora, zucchero, uova, farina, canditi',
+		'visibility' => true,
+	],
+	//italian 3
+	[
+		'name' => 'Pasta con parmigiano',
+		'ingredients' => 'parmigiano, olio Evo, sale,',
+		'visibility' => true,
+	],[
+		'name' => 'Risotto agli asparagi',
+		'ingredients' => 'asparagi, riso, parmigianoi, olio Evo, sale, pepe',
+		'visibility' => true,
+	],[
+		'name' => 'Misto di verdure',
+		'ingredients' => 'melanzane, zucchine, peperoni, cavolfiore, broccoli, pomodoro',
+		'visibility' => true,
+	],[
+		'name' => 'Pancotto',
+		'ingredients' => 'pane, rape, uova in camicia, olio Evo, sale, pepe',
+		'visibility' => false,
+	],[
+		'name' => 'Crespelle',
+		'ingredients' => 'uova, farina, burro, latte, panna, fughi, sale',
+		'visibility' => true,
+	],[
+		'name' => 'Omelette',
+		'ingredients' => 'uova, sale, olio Evo, pepe, parmigiano',
+		'visibility' => true,
+	],[
+		'name' => 'Merluzzo',
+		'ingredients' => 'merluzzo, pomodoro, olive, capperi, olio Evo, origano',
+		'visibility' => false,
+	],[
+		'name' => 'Trippa alla milanese',
+		'ingredients' => 'trippa, cipolla, lardo, sedano, aglio, carote, parmigiano',
+		'visibility' => true,
+	],[
+		'name' => 'Profitterol',
+		'ingredients' => 'uova, farina, zucchero, cacao amaro, panna, strega, latte, burro, cioccolato fondente',
+		'visibility' => true,
+	],[
+		'name' => 'Panna cotta',
+		'ingredients' => 'Panna, zucchero, latte, colla di pesce, caramello',
+		'visibility' => true,
+	],
+	//chinese 1
+	[
+		'name' => 'Riso alla cantonese',
+		'ingredients' => 'riso, piselli, prosciutto cotto, erba cipollina, uova',
+		'visibility' => true,
+	],[
+		'name' => 'Involtini primavera',
+		'ingredients' => 'carote, cavolfiore, cipolle, sale',
+		'visibility' => true,
+	],[
+		'name' => 'Pollo alle mandorle',
+		'ingredients' => 'pollo, mandorle, salsa di soia, fecola di patate, olio di arachidi, sale',
+		'visibility' => true,
+	],[
+		'name' => 'Spaghetti di riso ai frutti di mare',
+		'ingredients' => 'farina di riso, frutti di mare, salsa di soia, verdure',
+		'visibility' => true,
+	],[
+		'name' => 'Anatra alla pechinese',
+		'ingredients' => 'anatra, brodo, cipolla, zucchero, spezie, zenzero',
+		'visibility' => true,
+	],[
+		'name' => 'Nuvole di drago',
+		'ingredients' => 'gamberetti, farina di tapioca, sale, olio di arachidi',
+		'visibility' => false,
+	],[
+		'name' => 'Baozi',
+		'ingredients' => 'carne di maiale, cipollotto, cavolo, zenzero, salsa di soia, zucchero, sale, pepe',
+		'visibility' => true,
+	],[
+		'name' => 'Pollo al miele',
+		'ingredients' => 'pollo, salsa agrodolce, miele, cipollotto, semi di sesamo, salsa di soia',
+		'visibility' => true,
+	],[
+		'name' => 'Tang yuan',
+		'ingredients' => 'zenzero, fagioli rossi, sesamo, scorza di mandarino, riso',
+		'visibility' => true,
+	],[
+		'name' => 'Lou po bing',
+		'ingredients' => 'sesamo, melone, zucchero, farina di riso, cocco',
+		'visibility' => true,
+	],
+	//chinese 2
+	[
+		'name' => 'Germogli di bambù con funghi cinesi',
+		'ingredients' => 'funghi, germogli di bambù, zenzero, salsa di soia, sale, zucchero',
+		'visibility' => true,
+	],[
+		'name' => "Costine di maiale all'arancia",
+		'ingredients' => 'costine di maiale, arancia, zucchero, vino di riso cinese',
+		'visibility' => true,
+	],[
+		'name' => 'Maiale in agrodolce',
+		'ingredients' => 'maiale, salsa agrodolce, salsa di soia, cipollotto',
+		'visibility' => true,
+	],[
+		'name' => 'Tagliata di pollo con riso e mandorle ',
+		'ingredients' => 'pollo, limone, mandorle, salsa di soia',
+		'visibility' => true,
+	],[
+		'name' => 'Toast di gamberi',
+		'ingredients' => 'pane, sesamo, salsa di soia, gamberi, cipolle, amido di mais',
+		'visibility' => true,
+	],[
+		'name' => 'Nian gao',
+		'ingredients' => 'zucchero, riso glutinoso, amido',
+		'visibility' => true,
+	],[
+		'name' => 'Dragon candy',
+		'ingredients' => 'zucchero, noccioline, semi di sesamo, cocco',
+		'visibility' => true,
+	],[
+		'name' => 'Pasta di semi di loto',
+		'ingredients' => 'fiore di loto, zucchero, semi essiccati, olio vegetale',
+		'visibility' => false,
+	],[
+		'name' => 'Tang hulu',
+		'ingredients' => 'frutta candita',
+		'visibility' => true,
+	],[
+		'name' => 'Durian fritto',
+		'ingredients' => 'durian, olio, farina',
+		'visibility' => true,
+	],
+	//japanase 1
+	[
+		'name' => 'Onigiri',
+		'ingredients' => 'riso, alga, salmone',
+		'visibility' => true,
+	],[
+		'name' => 'Ramen',
+		'ingredients' => 'spaghetti di riso, uova, sesamo, miso, zenzero, lonza di maiale, cipollotto',
+		'visibility' => true,
+	],[
+		'name' => 'Tempura',
+		'ingredients' => 'gamberi, farina',
+		'visibility' => true,
+	],[
+		'name' => 'Nigiri',
+		'ingredients' => 'riso, pesce',
+		'visibility' => true,
+	],[
+		'name' => 'Shabu Shabu',
+		'ingredients' => 'fette di carne, verdure, funghi, tofu',
+		'visibility' => true,
+	],[
+		'name' => 'Uramaki',
+		'ingredients' => 'riso, pesce, alga, semi di girasole',
+		'visibility' => false,
+	],[
+		'name' => 'Soba',
+		'ingredients' => 'farina di grano saraceno, brodo di carne, salsa di soia',
+		'visibility' => true,
+	],[
+		'name' => 'Sashimi',
+		'ingredients' => 'pesce crudo',
+		'visibility' => true,
+	],[
+		'name' => 'Zuppa di miso',
+		'ingredients' => 'brodo, fagioli di miso, tofu, cipolle verdi',
+		'visibility' => true,
+	],[
+		'name' => 'Tako Yaki',
+		'ingredients' => 'polpo, zenzero, cipolle, salsa per Tako Yaki, maionese, fiocchi di tempura',
+		'visibility' => true,
+	],
+	//japanese 2
+	[
+		'name' => 'Temaki',
+		'ingredients' => 'riso, pesce, alga, uova',
+		'visibility' => true,
+	],[
+		'name' => 'Okonomiyaki',
+		'ingredients' => 'cavolfiore, farina, cipollotto, uova, gamberi, calamari',
+		'visibility' => true,
+	],[
+		'name' => 'Ghioza',
+		'ingredients' => 'farina, carne di maiale, cicoria, cipollotto fresco, salsa di soia, olio di semi',
+		'visibility' => false,
+	],[
+		'name' => 'Yaki udon',
+		'ingredients' => 'farina di riso, salsa di soia, zenzero, verdure',
+		'visibility' => true,
+	],[
+		'name' => 'Katsudon',
+		'ingredients' => 'carne di maiale, panko, salsa di soia, salsa mirin',
+		'visibility' => true,
+	],[
+		'name' => 'Yosenab',
+		'ingredients' => 'salsa di soia, verdure, carne mista',
+		'visibility' => false,
+	],[
+		'name' => 'Bistecca di Kobe',
+		'ingredients' => 'carne di Kobe',
+		'visibility' => true,
+	],[
+		'name' => 'Doraiaki',
+		'ingredients' => 'pancake, crema',
+		'visibility' => true,
+	],[
+		'name' => 'Dango',
+		'ingredients' => 'riso glutinoso, soia tostata, sciroppo di amido, fagioli rossi, uova, thè verde',
+		'visibility' => true,
+	],[
+		'name' => 'Tayaki',
+		'ingredients' => 'marmellata di fagioli rossi, farina',
+		'visibility' => true,
+	],
+	//indian 1
+	[
+		'name' => 'Samosa',
+		'ingredients' => 'patate, carne, piselli',
+		'visibility' => true,
+	],[
+		'name' => 'Thali',
+		'ingredients' => 'pane, salse',
+		'visibility' => true,
+	],[
+		'name' => 'Parantha',
+		'ingredients' => 'pane, spezie, patate, salse',
+		'visibility' => true,
+	],[
+		'name' => 'Panipuri',
+		'ingredients' => 'pane, chutney, riso soffiato, melograno',
+		'visibility' => true,
+	],[
+		'name' => 'Kulfi',
+		'ingredients' => 'zafferano, rose, mango',
+		'visibility' => false,
+	],[
+		'name' => 'Pollo al burro',
+		'ingredients' => 'pollo, burro, pomodori speziati',
+		'visibility' => true,
+	],
+	//indian 2
+	[
+		'name' => 'Lassi',
+		'ingredients' => 'yougurt, mango, zafferano, banana',
+		'visibility' => true,
+	],[
+		'name' => 'Bhelpuri',
+		'ingredients' => 'chicchi di riso, arachidi, verdure, spezie, melograno',
+		'visibility' => true,
+	],[
+		'name' => 'Pollo al curry',
+		'ingredients' => 'pollo, curry, yougurt, lenticchie',
+		'visibility' => true,
+	],[
+		'name' => 'Polpette al curry',
+		'ingredients' => 'carne mista, yougurt, noce moscata, latte, pane, curry',
+		'visibility' => true,
+	],[
+		'name' => 'Masala dosa',
+		'ingredients' => 'farina di riso, curry, patate, riso, cipolle',
+		'visibility' => true,
+	],[
+		'name' => 'Chaas',
+		'ingredients' => 'yougurt, sale, acqua, spezie miste',
+		'visibility' => true,
+	],
+	//indian 3
+	[
+		'name' => 'Chole bhature',
+		'ingredients' => 'pane fritto, spezie, ceci',
+		'visibility' => true,
+	],[
+		'name' => 'Daal',
+		'ingredients' => 'lenticchie, spicchi di aglio, olio di arachidi, peperoncini, zenzero',
+		'visibility' => true,
+	],[
+		'name' => 'Riso biryani',
+		'ingredients' => 'riso, cavolfiore, peperone, cavolo, cipolla, curcuma, anacardi',
+		'visibility' => true,
+	],[
+		'name' => 'Barfi al cocco',
+		'ingredients' => 'latte, zucchero, burro, aromi',
+		'visibility' => true,
+	],[
+		'name' => 'Laddoo',
+		'ingredients' => 'farina di ceci, zucchero, mandorle, anacardi',
+		'visibility' => true,
+	],[
+		'name' => 'Chutney di mango',
+		'ingredients' => 'mango verde, aglio, sale, zucchero, polvere di peperoncino',
+		'visibility' => true,
+	],
+	//international 1
+	[
+		'name' => 'Panino con hamburger',
+		'ingredients' => 'pane, carne di manzo, pomodori, insalata, cheddar, bacon, cipolle fritte',
+		'visibility' => true,
+	],[
+		'name' => 'Crepes alla nutella',
+		'ingredients' => 'farina, latte, uova, nutella, pecorino romano',
+		'visibility' => true,
+	],[
+		'name' => 'Pizza Margherita',
+		'ingredients' => 'farina, olio Evo, pomodoro, mozzarella, basilico',
+		'visibility' => true,
+	],[
+		'name' => 'Cheesecacke',
+		'ingredients' => 'formaggio fresco, panna, burro, marmellata, biscotti',
+		'visibility' => true,
+	],[
+		'name' => 'Fish and chips',
+		'ingredients' => 'pesce fritto, patatine fritte',
+		'visibility' => true,
+	],[
+		'name' => 'Zuppa di cipolle',
+		'ingredients' => 'cipolle, burro, farina, brodo, formaggio',
+		'visibility' => true,
+	],[
+		'name' => 'Escargot',
+		'ingredients' => 'lumache, burro, prezzemolo, aglio, cipolle',
+		'visibility' => true,
+	],[
+		'name' => 'Paella',
+		'ingredients' => 'riso, pesce misto, cipolle, zafferano, paprika, pomodoro',
+		'visibility' => true,
+	],[
+		'name' => 'Tortilla di patate',
+		'ingredients' => 'patate uova, cipolle, sale, olio',
+		'visibility' => true,
+	],[
+		'name' => 'Tacos',
+		'ingredients' => 'farina di mais, olio di semi, carne macinata, porro, pomodorini, formaggio',
+		'visibility' => false,
+	],
+	//international2
+	[
+		'name' => 'Insalata greca',
+		'ingredients' => 'feta, olive nere, cetrioli, origano, pomodori',
+		'visibility' => true,
+	],[
+		'name' => 'Zuppa di gamberi tailandese',
+		'ingredients' => 'latte di cocco, brodo vegetale, gamberi, erba cipollina, curry, salsa di pesce',
+		'visibility' => true,
+	],[
+		'name' => 'Porridge',
+		'ingredients' => "fiocchi d'avena, latte, sciroppo d'acero, vaniglia, frutti di bosco",
+		'visibility' => true,
+	],[
+		'name' => 'Insalata di patate tedesca',
+		'ingredients' => 'patate, cipolla rossa, erba cipollina, brodo, aceto, senape',
+		'visibility' => false,
+	],[
+		'name' => 'Pretzel',
+		'ingredients' => 'farina, acqua, zucchero, burro, sale grosso',
+		'visibility' => true,
+	],[
+		'name' => 'Gulash',
+		'ingredients' => 'manzo, patate, peperoni verdi, cipolle, brodo di carne, paprika, pomodori',
+		'visibility' => true,
+	],[
+		'name' => 'Pastrami',
+		'ingredients' => 'manzo, semi di coriandolo, chiodi di garofano, paprika dolce, cannella, semi di senape',
+		'visibility' => true,
+	],[
+		'name' => 'Tortillas',
+		'ingredients' => 'farina di mais, sale, acqua',
+		'visibility' => true,
+	],[
+		'name' => 'Coniglio in umido',
+		'ingredients' => 'coniglio, vino rosso, pancetta, funghi, spezie, patate, burro',
+		'visibility' => false,
+	],[
+		'name' => 'Quesadillas',
+		'ingredients' => 'farina di mais, formaggio, mais, pomodoro, erba cipollina, olio',
+		'visibility' => true,
+	],
+	//international 3
+	[
+		'name' => "Spaghetti all'Amatriciana",
+		'ingredients' => 'pomodori, guanciale, peperoncino fresco, olio Evo, pecorino romano',
+		'visibility' => true,
+	],[
+		'name' => 'Bbq ribs',
+		'ingredients' => 'costolertte di maiale, senape, aglio in polvere, paprika dolce, peperoncino, pepe, sale',
+		'visibility' => true,
+	],[
+		'name' => 'Krapfen alla crema',
+		'ingredients' => 'farina, latte, uova, zucchero, burro, limone, vaniglia',
+		'visibility' => true,
+	],[
+		'name' => 'Torta Sacher',
+		'ingredients' => 'farina, zucchero, burro, cioccolato, marmellata di albicocche, latte',
+		'visibility' => true,
+	],[
+		'name' => 'French Toast',
+		'ingredients' => 'pane, uova, latte, cannella, burro, confettura di mirtilli',
+		'visibility' => true,
+	],[
+		'name' => "Anatra all'arancia francese",
+		'ingredients' => 'anatra, arancia, cipolla, burro, brodo di carne, carote, rosmarino, zucchero',
+		'visibility' => true,
+	],[
+		'name' => 'Cupcake',
+		'ingredients' => 'burro, zucchero, farina, uova, vaniglia, latte, zucchero a velo',
+		'visibility' => true,
+	],[
+		'name' => 'Cipolle fritte',
+		'ingredients' => 'cipolle, farina, sale, uova, pepe',
+		'visibility' => false,
+	],[
+		'name' => 'kebab',
+		'ingredients' => 'carne di pollo, tacchino, aromi, paprika, cipolla, insalata, salsa allo yougurt',
+		'visibility' => true,
+	],[
+		'name' => 'Gnocchi di riso cinesi',
+		'ingredients' => 'farina di riso, carote, cipollotto, zenzero, olio di semi, salsa di soia',
+		'visibility' => true,
+	],
 ];
