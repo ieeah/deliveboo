2538887a7bd9944aaad27c77bd8e5c3b06ab18a7
ieeah <87276064+ieeah@users.noreply.github.com>
2022-03-04 15:21:36 +0100

    ok

--- resources/js/pages/Checkout.vue
+++ resources/js/pages/Checkout.vue
@@ -2,28 +2,52 @@
   <section class="container">
 
 <div class="row">
-    <div class="col-6">
-        <div class="window">
+    <div class="col-lg-6 col-12">
+        <div class="my-5 window">
             <div class="top-section">
               <span>Dati Ordine</span>
           </div>
-
-          <div><span>Content</span></div>
-
+          
+          <div class="form container">
           <div>
-          <a class="btn" href="">
+              <h5>Form content</h5>
+              <h5>Form content</h5>
+              <h5>Form content</h5>
+              <h5>Form content</h5>
+              <h5>Form content</h5>
+             
+
+          <div class="d-flex justify-content-between">
+          <a class="btn pay" href="">
               Procedi al pagamento
           </a>
 
-          <a href="">Annulla</a>
+          <a class="btn cancel"  href="">
+              Annulla
+          </a>
+         
+
           </div>
           
+          <!-- loader apparirà al click e error message dentro il form-->
+          <Loader />
+
+          <div class="error">
+            <div class="d-flex justify-content-center p-2">
+                <i class="fa-solid fa-circle-xmark"></i>
+                </div>
+            <h5>Messaggio di errore</h5>
+           <p >Non siamo stati in grado di procedere con il pagamento, verifica di aver inserito correttamente il numero della tua carta di credito.</p>
+          </div>
+          
+          </div>
+          </div>
+
         </div>
     </div>
 
-    <div class="col-6">
+    <div class="col-lg-6 col-12">
         <Cart />
-
     </div>
 
 </div>
@@ -34,10 +58,12 @@
 
 <script>
 import Cart from '../components/Cart'
+import Loader from '../components/Loader';
 export default {
 name:'Checkout',
 components:{
     Cart,
+    Loader,
 }
 }
 </script>
@@ -45,8 +71,7 @@ components:{
 <style scoped lang="scss">
 @import '../../sass/front.scss';
 .window{
-    width:300px;
-    height:300px;
+    width:400px;
     border:5px solid $orange;
     border-radius:10px;
     .top-section{
@@ -54,12 +79,41 @@ components:{
         height:25px;
         background-color:$orange;
         color:white;
+        padding:5px;
     }
-    .btn{
+    .form{
+        margin:10px;
+        .pay{
         background-color: $secondary-200;
+		color: $dark-700;
 		font-weight: bold;
-		text-decoration: none;
 		color: $dark-700;
     }
+        .cancel{
+        background-color: white;
+		text-decoration: none;
+		color: $primary-400;
+		color: $orange;
+    }
+    .pay,.cancel{
+        margin-top:10px;
+        text-decoration: none;
+    }
+    .error{
+        width:350px;
+        border:5px solid $orange;
+        border-radius:10px;
+        padding:10px;
+        text-align: center;
+        .fa-circle-xmark{
+            font-size:50px;
+            color:$red;
+        }
+        p{
+            padding:10px 0px;
+        }
+    }
+    }
+    
 }
 </style>
