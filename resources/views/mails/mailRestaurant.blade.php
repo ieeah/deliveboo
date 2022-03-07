f02f0955d53dfcc5b59a91f02772780ad2865452
dokkalemon <dottori.roberto@gmail.com>
2022-03-07 23:30:28 +0100

    mail send function and set controller

---
+++ resources/views/mails/mailRestaurant.blade.php
@@ -0,0 +1,20 @@
+# Hai Ricevuto un nuovo Ordine!
+
+Riepilogo Ordine:
+- {quantità} nome piatto
+- {quantità} nome piatto
+- {quantità} nome piatto
+- {quantità} nome piatto
+- {quantità} nome piatto
+- {quantità} nome piatto
+- {quantità} nome piatto
+
+## Prezzo Ordine: **{Total price}**
+
+
+Dati destinatario:
+- Nome: **nome_utente**
+- Cognome: **cognome_utente**
+- Indirizzo: **indirizzo_utente**
+- N. Telefono: **phone_utente**
+- Email: **email_utente**
