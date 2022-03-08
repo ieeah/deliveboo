26e80caf4661c97eeb585a9604c826bb46699039
ieeah <87276064+ieeah@users.noreply.github.com>
2022-03-09 00:09:38 +0100

    fixed links in seeders

--- config/restaurants.php
+++ config/restaurants.php
@@ -8,7 +8,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via cibium, 1',
 		'vat_number' => '5563248',
-		'thumb' => 'https://unsplash.com/photos/kC1-9Snr9hI',
+		'thumb' => 'https://images.unsplash.com/photo-1569230516306-5a8cb5586399?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80',
 	],
 	[
 		'name' => 'Betty\'s burger',
@@ -16,7 +16,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via hamburger, 1',
 		'vat_number' => '48241225',
-		'thumb' => 'https://unsplash.com/photos/F2GzZ08O6eQ',
+		'thumb' => 'https://images.unsplash.com/photo-1528680451339-3df1f6d40f38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2043&q=80',
 	],
 	[
 		'name' => 'Pastium',
@@ -24,7 +24,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via pasta, 1',
 		'vat_number' => '6358895',
-		'thumb' => 'https://unsplash.com/photos/q03ZLncun2Y',
+		'thumb' => 'https://images.unsplash.com/photo-1534650075489-3baecec1e8b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
 	],
 	[
 		'name' => 'Trattoria Lucana',
@@ -32,7 +32,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Lucana, 1',
 		'vat_number' => '25796451325',
-		'thumb' => 'https://unsplash.com/photos/BpugFbwxOfw',
+		'thumb' => 'https://images.unsplash.com/photo-1579926545913-448d525562db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80',
 	],
 	[
 		'name' => 'Dedalo',
@@ -40,7 +40,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Caduti di Nassiria, 14',
 		'vat_number' => '39846821486',
-		'thumb' => 'https://unsplash.com/photos/mYK3JCevRTk',
+		'thumb' => 'https://images.unsplash.com/photo-1617655699589-64375e5ad9a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
 	],
 	[
 		'name' => 'Dimora Ulmo',
@@ -48,7 +48,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Giuseppe Garibaldio, 68',
 		'vat_number' => '47869132578',
-		'thumb' => 'https://unsplash.com/photos/v4LQkXJPK0o',
+		'thumb' => 'https://images.unsplash.com/photo-1534425582704-65e021820688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
 	],
 	[
 		'name' => 'Dim Sum',
@@ -56,7 +56,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Arno, 45',
 		'vat_number' => '49832579849',
-		'thumb' => 'https://unsplash.com/photos/sHp2LtuC7Co',
+		'thumb' => 'https://images.unsplash.com/photo-1602421312666-5883f16a7c85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
 	],
 	[
 		'name' => 'Woko One',
@@ -64,7 +64,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Federico II, 36',
 		'vat_number' => '59831459725',
-		'thumb' => 'https://unsplash.com/photos/EHK-EH1SRzQ',
+		'thumb' => 'https://images.unsplash.com/photo-1502364271109-0a9a75a2a9df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
 	],
 	[
 		'name' => 'Sushi Koto',
@@ -72,7 +72,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Don Luigi Sturzo, 12',
 		'vat_number' => '45973256488',
-		'thumb' => 'https://unsplash.com/photos/a5ToDH34m0I',
+		'thumb' => 'https://images.unsplash.com/photo-1470256699805-a29e1b58598a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
 	],
 	[
 		'name' => 'Sushi Stone',
@@ -80,7 +80,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Taranto, 1',
 		'vat_number' => '25973654987',
-		'thumb' => 'https://unsplash.com/photos/BLV9FXiw9lw',
+		'thumb' => 'https://images.unsplash.com/photo-1629712257540-e03dfbd96b0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
 	],
 	[
 		'name' => 'Il Guru',
@@ -88,7 +88,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Carlo Levi, 78',
 		'vat_number' => '25791463258',
-		'thumb' => 'https://unsplash.com/photos/nmpW_WwwVSc',
+		'thumb' => 'https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
 	],
 	[
 		'name' => 'Tara',
@@ -96,7 +96,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Degli Aragonesi, 6',
 		'vat_number' => '47936578945',
-		'thumb' => 'https://unsplash.com/photos/yzM66Y_D3Dc',
+		'thumb' => 'https://images.unsplash.com/photo-1584819299780-0cb713a9ba58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
 	],
 	[
 		'name' => 'Il Naan',
@@ -104,7 +104,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Venezia, 27',
 		'vat_number' => '76148935478',
-		'thumb' => '',
+		'thumb' => 'https://images.unsplash.com/photo-1586999768265-24af89630739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHJlc3RhdXJhbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
 	],
 	[
 		'name' => 'Snack House',
@@ -112,7 +112,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Dei Pesci, 67',
 		'vat_number' => '47525836974',
-		'thumb' => 'https://unsplash.com/photos/1H30uRC1plc',
+		'thumb' => 'https://images.unsplash.com/photo-1590846406792-0adc7f938f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1285&q=80',
 	],
 	[
 		'name' => 'Bloody Mary ',
@@ -120,7 +120,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Riscatto, 54',
 		'vat_number' => '25473698754',
-		'thumb' => 'https://unsplash.com/photos/S6atLH5Rf0U',
+		'thumb' => 'https://images.unsplash.com/photo-1572116469696-31de0f17cc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
 	],
 	[
 		'name' => 'Gambero Rosso',
@@ -128,7 +128,7 @@ return [
 		'password' => '12345678',
 		'address' => 'Via Tevere, 19',
 		'vat_number' => '24836972458',
-		'thumb' => 'https://unsplash.com/photos/jTzT09l-nw4',
+		'thumb' => 'https://images.unsplash.com/photo-1589704379319-3b83010f661e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80',
 	],
 	
 ];
