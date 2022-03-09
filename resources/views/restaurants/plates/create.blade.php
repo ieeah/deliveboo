2f6699014fec4cc941c6f3c734dbbb27dc33462c
Aurora Grippaudo <aurora268@hotmail.it>
2022-03-09 16:34:28 +0100

    sistemato card piatti e bottoni

--- resources/views/restaurants/plates/create.blade.php
+++ resources/views/restaurants/plates/create.blade.php
@@ -1,67 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container plates_create_page">
-		<div class="heading">
-			<h1>
-				{{Auth::user()->name}}
-			</h1>
-			<h2>
-				Aggiungi un nuovo piatto al menù
-			</h2>
-		</div>
+    <div class="container plates_create_page">
+        <div class="heading">
+            <h1>
+                {{ Auth::user()->name }}
+            </h1>
+            <h2>
+                Aggiungi un nuovo piatto al menù
+            </h2>
+        </div>
 
-		<form action="{{route('restaurants.plates.store')}}"
-			class="d-flex flex-column"
-			method="POST" enctype="multipart/form-data">
-			@csrf
-			<div class="mb-4">
-				<label for="name">Nome Piatto *</label>
-				<input class="form-control" type="text" name="name" id="name" value="{{ old('title') }}" required minlength="2" maxlength="200">
-				@error('name')
-					<div class="text-danger fw-bold">{{ $message }}</div>
-				@enderror
-			</div>
-			<div class="mb-4">
-				<label for="description">Descrizione</label>
-				<textarea class="form-control" type="text" name="description" id="description" maxlength="1000">{{ old('description') }}</textarea>
-				@error('description')
-					<div class="text-danger fw-bold">{{ $message }}</div>
-				@enderror
-			</div>
+        <form action="{{ route('restaurants.plates.store') }}" class="d-flex flex-column" method="POST"
+            enctype="multipart/form-data">
+            @csrf
+            <div class="mb-4">
+                <label for="name">Nome Piatto *</label>
+                <input class="form-control" type="text" name="name" id="name" value="{{ old('title') }}" required
+                    minlength="2" maxlength="200">
+                @error('name')
+                    <div class="text-danger fw-bold">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-4">
+                <label for="description">Descrizione</label>
+                <textarea class="form-control" type="text" name="description" id="description"
+                    maxlength="1000">{{ old('description') }}</textarea>
+                @error('description')
+                    <div class="text-danger fw-bold">{{ $message }}</div>
+                @enderror
+            </div>
 
-			<div class="mb-4">
-				<label for="ingredients">Ingredienti *</label>
-				<textarea class="form-control" type="text" name="ingredients" id="ingredients" required maxlength="1000">{{ old('ingredients') }}</textarea>
-				@error('ingredients')
-					<div class="text-danger fw-bold">{{ $message }}</div>
-				@enderror
-			</div>
+            <div class="mb-4">
+                <label for="ingredients">Ingredienti *</label>
+                <textarea class="form-control" type="text" name="ingredients" id="ingredients" required
+                    maxlength="1000">{{ old('ingredients') }}</textarea>
+                @error('ingredients')
+                    <div class="text-danger fw-bold">{{ $message }}</div>
+                @enderror
+            </div>
 
-			<div class="mb-4">
-				<label for="price">Prezzo *</label>
-				<input class="form-control" type="number" name="price" id="price" min="1" max="99.90" step="0.1" required>
-			</div>
+            <div class="mb-4">
+                <label for="price">Prezzo *</label>
+                <input class="form-control" type="number" name="price" id="price" min="1" max="99.90" step="0.1" required>
+            </div>
 
-			<div class="mb-4">
-				<label for="visibility" class="d-block">Disponibile</label>
-				<input type="checkbox" name="visibility" id="visibility" checked>
-			</div>
+            <div class="mb-4">
+                <label for="visibility" class="d-block">Disponibile</label>
+                <input type="checkbox" name="visibility" id="visibility" checked>
+            </div>
 
-			<div class="mb-4">
-				<label for="thumb">Cover Image</label>
-				<p>solo file png/jpg</p>
-				<input class="form-control-file" type="file" name="thumb" id="thumb" accept="image/png, image/jpeg">
-				@error('thumb')
-					<div class="text-danger">{{$message}}</div>
-				@enderror
-			</div>
+            <div class="mb-4">
+                <label for="thumb">Cover Image</label>
+                <p>solo file png/jpg</p>
+                <input class="form-control-file" type="file" name="thumb" id="thumb" accept="image/png, image/jpeg">
 
-			<input type="submit" value="Salva nuovo piatto" class="mt-4 btn btn-success">
-		</form>
+                @error('thumb')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
 
-		<div class="actions d-flex justify-content-end">
-			<a class="btn btn-secondary mt-3" href="{{ route('restaurants.plates.index') }}">Annulla</a>
-		</div>
-	</div>
+            <input type="submit" value="Salva nuovo piatto" class="mt-4 w-25 text-center btn btn-success">
+        </form>
+
+        <div class="actions d-flex justify-content-end">
+            <a class="btn btn-secondary mt-3" href="{{ route('restaurants.plates.index') }}">Annulla</a>
+        </div>
+    </div>
 @endsection
--- resources/views/restaurants/plates/edit.blade.php
+++ resources/views/restaurants/plates/edit.blade.php
@@ -1,93 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container plates_create_page">
-		<div class="heading">
-			<h1>
-				{{Auth::user()->name}}
-			</h1>
-			<h2>
-				Modifica {{$plate->name}}
-			</h2>
-		</div>
+    <div class="container plates_create_page">
+        <div class="heading">
+            <h1>
+                {{ Auth::user()->name }}
+            </h1>
+            <h2>
+                Modifica {{ $plate->name }}
+            </h2>
+        </div>
 
-		<form action="{{route('restaurants.plates.update', $plate->id)}}"
-			class="d-flex flex-column"
-			method="POST" enctype="multipart/form-data">
-			@csrf
-			@method('PATCH')
+        <form action="{{ route('restaurants.plates.update', $plate->id) }}" class="d-flex flex-column" method="POST"
+            enctype="multipart/form-data">
+            @csrf
+            @method('PATCH')
 
-			@if ($errors->any())
-				@foreach ($errors->all() as $error)
-					<div class="text-danger">{{$error}}</div>
-				@endforeach
-			@endif
+            @if ($errors->any())
+                @foreach ($errors->all() as $error)
+                    <div class="text-danger">{{ $error }}</div>
+                @endforeach
+            @endif
 
-			<div class="mb-4">
-				<label for="name">Nome Piatto *</label>
-				<input class="form-control" type="text" name="name" id="name" value="{{ old('title', $plate->name) }}" 
-				{{-- required minlength="2" maxlength="200" --}}
-				>
-				@error('name')
-					<div class="text-danger fw-bold">{{ $message }}</div>
-				@enderror
-			</div>
-			<div class="mb-4">
-				<label for="description">Descrizione</label>
-				<textarea class="form-control" type="text" name="description" id="description" 
-				{{-- maxlength="1000" --}}
-				>{{ old('description', $plate->description) }}</textarea>
-				@error('description')
-					<div class="text-danger fw-bold">{{ $message }}</div>
-				@enderror
-			</div>
+            <div class="mb-4">
+                <label for="name">Nome Piatto *</label>
+                <input class="form-control" type="text" name="name" id="name" value="{{ old('title', $plate->name) }}"
+                    {{-- required minlength="2" maxlength="200" --}}>
+                @error('name')
+                    <div class="text-danger fw-bold">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-4">
+                <label for="description">Descrizione</label>
+                <textarea class="form-control" type="text" name="description" id="description"
+                    {{-- maxlength="1000" --}}>{{ old('description', $plate->description) }}</textarea>
+                @error('description')
+                    <div class="text-danger fw-bold">{{ $message }}</div>
+                @enderror
+            </div>
 
-			<div class="mb-4">
-				<label for="ingredients">Ingredienti *</label>
-				<textarea class="form-control" type="text" name="ingredients" id="ingredients" 
-				{{-- maxlength="1000" required --}}
-				>{{ old('ingredients', $plate->ingredients) }}</textarea>
-				@error('ingredients')
-					<div class="text-danger fw-bold">{{ $message }}</div>
-				@enderror
-			</div>
+            <div class="mb-4">
+                <label for="ingredients">Ingredienti *</label>
+                <textarea class="form-control" type="text" name="ingredients" id="ingredients"
+                    {{-- maxlength="1000" required --}}>{{ old('ingredients', $plate->ingredients) }}</textarea>
+                @error('ingredients')
+                    <div class="text-danger fw-bold">{{ $message }}</div>
+                @enderror
+            </div>
 
-			<div class="mb-4">
-				<label for="price">Prezzo *</label>
-				<input class="form-control" step="0.1" name="price" id="price" type="number"
-				{{-- min="0" max="99.90" required   --}}
-				value="{{ old('price', $plate->price)}}">
-				@error('price')
-					<div class="text-danger">{{$message}}</div>
-				@enderror
-			</div>
+            <div class="mb-4">
+                <label for="price">Prezzo *</label>
+                <input class="form-control" step="0.1" name="price" id="price" type="number" {{-- min="0" max="99.90" required --}}
+                    value="{{ old('price', $plate->price) }}">
+                @error('price')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
 
-			<div class="mb-4">
-				<label for="visibility" class="d-block">Disponibile</label>
-				<input type="checkbox" name="visibility" id="visibility" @if ($plate->visibility) checked @endif>
-			</div>
+            <div class="mb-4">
+                <label for="visibility" class="d-block">Disponibile</label>
+                <input type="checkbox" name="visibility" id="visibility" @if ($plate->visibility) checked @endif>
+            </div>
 
-			<div class="mb-4">
-				<label for="thumb">Cover Image</label>
-				@if ($plate->thumb)
-				<figure class="mb-3">
-					<img width="300" src="{{asset('storage/' . $plate->thumb)}}" alt="{{$plate->thumb}}">
-				</figure>
-				@endif
-				<input class="form-control-file" type="file" name="thumb" id="thumb"
-				accept="image/png, image/jpeg, image/jpg"
-				>
-				@error('thumb')
-					<div class="text-danger">{{$message}}</div>
-				@enderror
-			</div>
+            <div class="mb-4">
+                <label for="thumb">Cover Image</label>
+                @if ($plate->thumb)
+                    <figure class="mb-3">
+                        <img width="300" src="{{ asset('storage/' . $plate->thumb) }}" alt="{{ $plate->thumb }}">
+                    </figure>
+                @endif
+                <input class="form-control-file" type="file" name="thumb" id="thumb"
+                    accept="image/png, image/jpeg, image/jpg">
+                @error('thumb')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
 
-			{{-- TODO - verificare se devo implementare altro e come nel controller per la gestione della foto --}}
-			<input type="submit" value="Modifica piatto" class="mt-4 btn btn-success">
-		</form>
+            {{-- TODO - verificare se devo implementare altro e come nel controller per la gestione della foto --}}
+            <input type="submit" value="Modifica piatto" class="mt-4 w-25 btn btn-success">
+        </form>
 
-		<div class="actions d-flex justify-content-end">
-			<a class="btn btn-secondary mt-3" href="{{ route('restaurants.plates.index') }}">Annulla</a>
-		</div>
-	</div>
+        <div class="actions d-flex justify-content-end">
+            <a class="btn btn-secondary mt-3" href="{{ route('restaurants.plates.index') }}">Annulla</a>
+        </div>
+    </div>
 @endsection
